513637a0b47c736d4cfdad759c23bee9184f2870
Bencooo <bentalha270@outlook.fr>
2025-02-05 15:27:24 +0100

    update internal, to public

--- src/BencoToken.sol
+++ src/BencoToken.sol
@@ -4,12 +4,12 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract BencoToken is ERC20, Ownable {
+contract BencoToken is ERC20 {
     constructor(uint256 initialSupply) ERC20("BencoToken", "BENCO") {
         _mint(msg.sender, initialSupply * 10 ** decimals());
     }
 
-    function mint(address to, uint256 amount) external onlyOwner {
+    function mint(address to, uint256 amount) public {
         _mint(to, amount);
     }
 
